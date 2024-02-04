59a89f538ffe71178c068badb53cecd7a0c0ed3a
seifeddin <seif1994@yahoo.fr>
2024-02-04 17:06:53 +0100

    database seed and display avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+
+# Création de personnages
+4.times do
+  character = Character.create!(
+    name: Faker::Games::Witcher.character,
+    life_points: Faker::Number.between(from: 10, to: 100),
+    attack_points: Faker::Number.between(from: 1, to: 10)
+  )
+  avatar_url = Faker::Avatar.image(slug: character.name.parameterize, size: '100x100', format: 'jpg')
+  avatar_file = URI.open(avatar_url)
+  character.avatar.attach(io: avatar_file, filename: "#{character.name.parameterize}-avatar.jpg", content_type: 'image/jpeg')
+end
+
+# Création d'armes
+4.times do
+  Weapon.create!(
+    name: Faker::Games::ElderScrolls.weapon ,
+    attack_bonus: Faker::Number.between(from: 0, to: 30)
+  )
+end
+
+# Création de boucliers
+4.times do
+  Shield.create!(
+    name: Faker::Games::ClashOfClans.defensive_building,
+    defense_bonus: Faker::Number.between(from: 0, to: 30)
+  )
+end
+
+puts 'Database populated with characters, weapons, and shields!'
